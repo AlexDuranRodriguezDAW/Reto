c5ca5a61714472a57cf8fded641cce3949351543
Alex Durán <alex.duran@ikasle.egibide.org>
2023-05-05 09:46:31 +0200

    Creacion de las clases relacionadas con persona

--- Programacion/KingsLeague/src/Modelo/Propietario.java
+++ Programacion/KingsLeague/src/Modelo/Propietario.java
@@ -1,4 +1,10 @@
 package Modelo;
 
 public class Propietario extends Persona{
+    public Propietario() {
+    }
+
+    public Propietario(String id, String dni, String apellidos, String nombre) {
+        super(id, dni, apellidos, nombre);
+    }
 }
--- Programacion/KingsLeague/src/Modelo/Staff.java
+++ Programacion/KingsLeague/src/Modelo/Staff.java
@@ -1,4 +1,10 @@
 package Modelo;
 
 public class Staff extends Persona{
+    public Staff() {
+    }
+
+    public Staff(String id, String dni, String apellidos, String nombre) {
+        super(id, dni, apellidos, nombre);
+    }
 }
