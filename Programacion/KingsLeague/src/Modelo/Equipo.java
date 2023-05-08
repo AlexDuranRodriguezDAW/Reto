ac20ada073b95278efa87569360e1d7429d9e9a4
Aritz Saiz <aritz.saiz@ikasle.egibide.org>
2023-05-08 12:28:09 +0200

    clases

--- Programacion/KingsLeague/src/Modelo/Equipo.java
+++ Programacion/KingsLeague/src/Modelo/Equipo.java
@@ -1,4 +1,38 @@
 package Modelo;
 
 public class Equipo {
+
+    private int idEquipo;
+    private String nombreEquipo;
+    private Double presupuesto;
+
+    public Equipo(int idEquipo, String nombreEquipo, Double presupuesto) {
+        this.idEquipo = idEquipo;
+        this.nombreEquipo = nombreEquipo;
+        this.presupuesto = presupuesto;
+    }
+
+    public int getIdEquipo() {
+        return idEquipo;
+    }
+
+    public void setIdEquipo(int idEquipo) {
+        this.idEquipo = idEquipo;
+    }
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public Double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(Double presupuesto) {
+        this.presupuesto = presupuesto;
+    }
 }
