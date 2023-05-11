048d2cffb83e1312b3e363252690c9afe11ff307
Aritz Saiz <aritz.saiz@ikasle.egibide.org>
2023-05-11 11:47:00 +0200

    tablas

---
+++ Base_de_datos/Paquete_Calendario_Header.sql
@@ -0,0 +1,10 @@
+
+-- PAQUETE REFERENTE AL CALENDARIO (HEADER) --
+
+CREATE OR REPLACE PACKAGE DatosCalendario IS
+
+    PROCEDURE EmparejarLigaRegular;
+    
+    PROCEDURE EmparejarPLayOff;
+
+END DatosCalendario;
---
+++ Base_de_datos/ejecutableDeSql.sql
@@ -0,0 +1,9 @@
+ 
+
+ @/Users/Usuario/Desktop/Reto/Base_de_Datos/creacionTablas.sql
+ @/Users/Usuario/Desktop/Reto/Base_de_Datos/Vistas.sql
+ @/Users/Usuario/Desktop/Reto/Base_de_Datos/JuegoDeDatos.sql
+ @/Users/Usuario/Desktop/Reto/Base_de_Datos/triggers.sql
+ @/Users/Usuario/Desktop/Reto/Base_de_Datos/Paquete_Calendario_Header.sql
+ @/Users/Usuario/Desktop/Reto/Base_de_Datos/Paquete_Calendario_Body.sql
+ @/Users/Usuario/Desktop/Reto/Base_de_Datos/Procedimientos_almacenados.sql
