a3fd7424341d7760495dbc1cc2698523bd7d77ce
Jorge Aparicio <94176866+JorgeAparicioFernandez@users.noreply.github.com>
2023-05-15 12:33:51 +0200

    Cuerpo del procedimiento casi acabado.
    Cuerpo del procedimiento con la generacion de las jornadas casi acabada, tiene un error a la hora de conseguir emparejar a todos en una misma jornada a partir de la jornada 2.

--- Base_de_datos/Paquete_Calendario_Body.sql
+++ Base_de_datos/Paquete_Calendario_Body.sql
@@ -10,106 +10,161 @@ CREATE OR REPLACE PACKAGE BODY DatosCalendario AS
     PROCEDURE EmparejarLigaRegular
     AS
     
-    V_FechaJornada  DATE := SYSDATE;
-    V_HoraPartido   NUMBER := 10.00;
-    V_CursorJornada SYS_REFCURSOR;
-    V_CursorEquipo1 SYS_REFCURSOR;
-    V_CursorEquipo2 SYS_REFCURSOR;
-    V_IdJornada     JORNADAS.ID%TYPE;
-    V_IdEquipo1     EQUIPOS.ID%TYPE;
-    V_IdEquipo2     EQUIPOS.ID%TYPE;
-    V_PartidoExiste NUMBER(2);
+    V_FechaJornada      DATE := SYSDATE;
+    V_HoraPartido       VARCHAR(5) := '1600';
+    V_IdSplit           SPLIT.ID%TYPE;
+    V_IdJornada         JORNADAS.ID%TYPE;
+    V_IdJornadaMinima   JORNADAS.ID%TYPE;
+    V_IdPartido         PARTIDOS.ID%TYPE;
+    V_IdEquipo1         EQUIPOS.ID%TYPE;
+    V_IdEquipo2         EQUIPOS.ID%TYPE;
+    V_PartidoExiste     NUMBER;
+    V_Emparejados       NUMBER;
     
     BEGIN
     
-        OPEN V_CursorEquipo1 FOR
-        SELECT ID FROM EQUIPOS;
-        
-        OPEN V_CursorEquipo2 FOR
-        SELECT ID FROM EQUIPOS;
-    
     --CREAR 11 JORNADAS PARA EL ULTIMO SPLIT
+        
+        SELECT MAX(ID) INTO V_IdSplit
+        FROM SPLIT;
+        
         FOR NumJornadas IN 1..11
         LOOP
         
         INSERT INTO JORNADAS (FECHA, TIPO, IDSPLIT)
-        VALUES(V_FechaJornada, 'regular', (SELECT MAX(ID) FROM SPLIT));
+        VALUES(V_FechaJornada, 'regular', V_IdSplit);
         
         V_FechaJornada := V_FechaJornada + 7;
         
         END LOOP;
     
     --OBTENER LOS ID DE LAS JORNADAS
-        OPEN V_CursorJornada FOR
-        SELECT ID FROM JORNADAS;
+        SELECT MIN(ID) INTO V_IdJornada
+        FROM JORNADAS
+        WHERE IDSPLIT = V_IdSplit;
         
     --CREAR LOS PARTIDOS PARA CADA JORNADA
+    
+        V_Emparejados := 1;
+        
+        
+        
+        FOR EQUIPO1 IN (SELECT * FROM EQUIPOS) 
         LOOP
         
-            FETCH V_CursorJornada INTO V_IdJornada;
+            SELECT MIN(ID) INTO V_IdJornada
+            FROM JORNADAS
+            WHERE IDSPLIT = V_IdSplit;
             
-            EXIT WHEN V_CursorJornada%NOTFOUND;
-            
-            INSERT INTO PARTIDOS (HORA, IDJORNADA)
-            VALUES (TO_CHAR(V_HoraPartido), V_IdJOrnada);
-        
-            V_HoraPartido := V_HoraPartido + 02.00;
-        
-        --CREAR LOS 3 EMPAREJAMIENTOS
-            FOR NumPartidos IN 1..6
+            FOR EQUIPO2 IN (SELECT * FROM EQUIPOS)
             LOOP
             
-                FETCH V_CursorEquipo1 INTO V_IdEquipo1;
-                
-                LOOP
-                
-                    FETCH V_CursorEquipo2 INTO V_IdEquipo2;
-                    EXIT WHEN V_IdEquipo2 != V_IdEquipo1;
-                
-                END LOOP;
-                
-                IF V_IdEquipo1 != V_IdEquipo2 THEN
-                
-                SELECT NVL(COUNT(ID),0) INTO V_PartidoExiste
-                FROM EMPAREJAMIENTOS
-                WHERE ID =
-                       (SELECT ID 
-                        FROM EMPAREJAMIENTOS
-                        WHERE EQUIPO1 = V_IdEquipo1 AND EQUIPO2 = V_IdEquipo2
-                        OR EQUIPO1 = V_IdEquipo2 AND EQUIPO2 = V_IdEquipo1
-                        OR EQUIPO1 = V_IdEquipo1 OR EQUIPO2 = V_IdEquipo2
-                        OR EQUIPO1 = v_IdEquipo1 OR EQUIPO2 = V_IdEquipo1);
-                
-                
-                
-                IF V_partidoExiste != 0 THEN  
-                
-                INSERT INTO Emparejamientos(ID, Equipo1, Equipo2)
-                VALUES (V_IdJornada, V_IdEquipo1, V_IdEquipo2);
-                
-                END IF;
-                
-                END IF;
-                
-                
+            SELECT COUNT(P.ID) INTO V_PartidoExiste
+                FROM PARTIDOS P
+                WHERE (P.ID IN (SELECT ID
+                                FROM PARTIDOEQUIPO1
+                                WHERE IDEQUIPO = EQUIPO1.ID
+                                AND IDPARTIDO IN (SELECT ID
+                                               FROM PARTIDOS
+                                               WHERE IDJORNADA = V_IdJornada)))
+                OR (P.ID IN (SELECT ID
+                             FROM PARTIDOEQUIPO2
+                             WHERE IDEQUIPO = EQUIPO1.ID
+                             AND IDPARTIDO IN (SELECT ID
+                                               FROM PARTIDOS
+                                               WHERE IDJORNADA = V_IdJornada)));
             
-            CLOSE V_CursorEquipo1;
-            CLOSE V_CursorEquipo2;
+            IF V_PartidoExiste = 0 THEN
             
-            OPEN V_CursorEquipo1 FOR
-            SELECT ID FROM EQUIPOS;
+            IF EQUIPO1.ID <> EQUIPO2.ID THEN
+                
+                SELECT COUNT(P.ID) INTO V_PartidoExiste
+                FROM PARTIDOS P
+                WHERE (P.ID IN (SELECT ID
+                                FROM PARTIDOEQUIPO1
+                                WHERE IDEQUIPO = EQUIPO1.ID
+                                AND IDPARTIDO IN (SELECT IDPARTIDO
+                                                  FROM PARTIDOEQUIPO2
+                                                  WHERE IDEQUIPO = EQUIPO2.ID)))
+                OR (P.ID IN (SELECT ID
+                             FROM PARTIDOEQUIPO2
+                             WHERE IDEQUIPO = EQUIPO1.ID
+                             AND IDPARTIDO IN (SELECT IDPARTIDO
+                                               FROM PARTIDOEQUIPO1
+                                               WHERE IDEQUIPO = EQUIPO2.ID)))
+                OR (P.ID IN (SELECT ID
+                             FROM PARTIDOEQUIPO1
+                             WHERE IDEQUIPO = EQUIPO2.ID
+                             AND IDPARTIDO IN (SELECT ID
+                                               FROM PARTIDOS
+                                               WHERE IDJORNADA = V_IdJornada)))
+                OR (P.ID IN (SELECT ID
+                             FROM PARTIDOEQUIPO2
+                             WHERE IDEQUIPO = EQUIPO2.ID
+                             AND IDPARTIDO IN (SELECT ID
+                                               FROM PARTIDOS
+                                               WHERE IDJORNADA = V_IdJornada)));
         
-            OPEN V_CursorEquipo2 FOR
-            SELECT ID FROM EQUIPOS;
+                IF V_PartidoExiste = 0 THEN
+                    
+                    V_IdEquipo1 := EQUIPO1.ID;
+                    V_IdEquipo2 := EQUIPO2.ID;
+                    
+                    INSERT INTO PARTIDOS (HORA, IDJORNADA) VALUES 
+                    (V_HoraPartido, V_IdJornada);
+                    
+                    SELECT ID INTO V_IdPartido
+                    FROM PARTIDOS 
+                    WHERE ROWNUM = 1 
+                    ORDER BY ID DESC;
+                    
+                    INSERT INTO PARTIDOEQUIPO1 (IDEQUIPO, IDPARTIDO)
+                    VALUES (V_IdEquipo1, V_IdPartido);
+                    
+                    INSERT INTO PARTIDOEQUIPO2 (IDEQUIPO, IDPARTIDO)
+                    VALUES (V_IdEquipo2, V_IdPartido);
+                    
+                    V_Emparejados := 0;
+                    
+                        IF V_Emparejados = 0 THEN
+                        
+                            V_IdJornada := V_IdJornada + 1;
+                            V_Emparejados := 1;
+                            
+                        END IF;
+                END IF;
+                
+            END IF;
+            ELSE
+            
+            V_IdJornada := V_IdJornada + 1;
+            
+            END IF;
             
             END LOOP;
-        
+            
+            
+            
         END LOOP;
-    
-    CLOSE V_CursorEquipo1;
-    CLOSE V_CursorEquipo2;
-    
+        
     END EmparejarLigaRegular;
+
+--------------------------------------------------------------------------------
+--****************************************************************************--
+--------------------------------------------------------------------------------
+
+    PROCEDURE ListaEmparejamientos
+    (C_CURSOR OUT SYS_REFCURSOR)
+    AS
+    BEGIN
+        OPEN C_CURSOR FOR
+                SELECT P.ID, EL.NOMBRE AS LOCAL, EV.NOMBRE AS VISITANTE, P.ID, P.HORA
+                FROM PARTIDOS P, PARTIDOEQUIPO1 PL, PARTIDOEQUIPO2 PV, EQUIPOS EL, EQUIPOS EV
+                WHERE P.ID = PL.IDPARTIDO AND P.ID = PV.IDPARTIDO
+                AND PL.IDEQUIPO = EL.ID AND PV.IDEQUIPO = EV.ID
+                AND P.ID IN (SELECT ID FROM JORNADAS WHERE IDSPLIT = (SELECT max(ID) from SPLIT))
+                GROUP BY P.ID, EL.NOMBRE, EV.NOMBRE, P.IDJORNADA, P.HORA;
+    END ListaEmparejamientos;
 
 --------------------------------------------------------------------------------
 --****************************************************************************--
