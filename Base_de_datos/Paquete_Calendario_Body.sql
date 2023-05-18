12fa1ba14685852a71de26a0734a3f097f0a0b2c
Jorge Aparicio <jorge.aparicio@ikasle.egibide.org>
2023-05-18 12:46:21 +0200

    Corrección Masiva SQL (triggers, vistas...)

--- Base_de_datos/Paquete_Calendario_Body.sql
+++ Base_de_datos/Paquete_Calendario_Body.sql
@@ -14,7 +14,6 @@ CREATE OR REPLACE PACKAGE BODY DatosCalendario AS
     V_HoraPartido       VARCHAR(5) := '16:00';
     V_IdSplit           SPLIT.ID%TYPE;
     V_IdJornada         JORNADAS.ID%TYPE;
-    V_IdJornadaMinima   JORNADAS.ID%TYPE;
     V_IdPartido         PARTIDOS.ID%TYPE;
     V_IdEquipo1         EQUIPOS.ID%TYPE;
     V_IdEquipo2         EQUIPOS.ID%TYPE;
@@ -60,19 +59,9 @@ CREATE OR REPLACE PACKAGE BODY DatosCalendario AS
             LOOP
             
             SELECT COUNT(P.ID) INTO V_PartidoExiste
-                FROM PARTIDOS P
-                WHERE (P.ID IN (SELECT ID
-                                FROM PARTIDOEQUIPO1
-                                WHERE IDEQUIPO = EQUIPO1.ID
-                                AND IDPARTIDO IN (SELECT ID
-                                               FROM PARTIDOS
-                                               WHERE IDJORNADA = V_IdJornada)))
-                OR (P.ID IN (SELECT ID
-                             FROM PARTIDOEQUIPO2
-                             WHERE IDEQUIPO = EQUIPO1.ID
-                             AND IDPARTIDO IN (SELECT ID
-                                               FROM PARTIDOS
-                                               WHERE IDJORNADA = V_IdJornada)));
+            FROM PARTIDOS P
+            WHERE IDEQUIPO1 = EQUIPO1.ID AND IDJORNADA = V_IdJornada
+            OR IDEQUIPO2 = EQUIPO1.ID AND IDJORNADA = V_IdJornada;
             
             IF V_PartidoExiste = 0 THEN
             
@@ -80,49 +69,18 @@ CREATE OR REPLACE PACKAGE BODY DatosCalendario AS
                 
                 SELECT COUNT(P.ID) INTO V_PartidoExiste
                 FROM PARTIDOS P
-                WHERE (P.ID IN (SELECT ID
-                                FROM PARTIDOEQUIPO1
-                                WHERE IDEQUIPO = EQUIPO1.ID
-                                AND IDPARTIDO IN (SELECT IDPARTIDO
-                                                  FROM PARTIDOEQUIPO2
-                                                  WHERE IDEQUIPO = EQUIPO2.ID)))
-                OR (P.ID IN (SELECT ID
-                             FROM PARTIDOEQUIPO2
-                             WHERE IDEQUIPO = EQUIPO1.ID
-                             AND IDPARTIDO IN (SELECT IDPARTIDO
-                                               FROM PARTIDOEQUIPO1
-                                               WHERE IDEQUIPO = EQUIPO2.ID)))
-                OR (P.ID IN (SELECT ID
-                             FROM PARTIDOEQUIPO1
-                             WHERE IDEQUIPO = EQUIPO2.ID
-                             AND IDPARTIDO IN (SELECT ID
-                                               FROM PARTIDOS
-                                               WHERE IDJORNADA = V_IdJornada)))
-                OR (P.ID IN (SELECT ID
-                             FROM PARTIDOEQUIPO2
-                             WHERE IDEQUIPO = EQUIPO2.ID
-                             AND IDPARTIDO IN (SELECT ID
-                                               FROM PARTIDOS
-                                               WHERE IDJORNADA = V_IdJornada)));
+                WHERE   IDEQUIPO1 = EQUIPO1.ID AND IDEQUIPO2 = EQUIPO2
+                OR IDEQUIPO1 = EQUIPO2.ID AND IDEQUIPO2 = EQUIPO1
+                OR IDEQUIPO1 = EQUIPO2.ID AND IDJORNADA = V_IdJornada
+                OR IDEQUIPO1 = EQUIPO2.ID AND IDJORNADA = V_IdJornada;
         
                 IF V_PartidoExiste = 0 THEN
                     
                     V_IdEquipo1 := EQUIPO1.ID;
                     V_IdEquipo2 := EQUIPO2.ID;
                     
-                    INSERT INTO PARTIDOS (HORA, IDJORNADA) VALUES 
-                    (V_HoraPartido, V_IdJornada);
-                    
-                    SELECT ID INTO V_IdPartido
-                    FROM PARTIDOS 
-                    WHERE ROWNUM = 1 
-                    ORDER BY ID DESC;
-                    
-                    INSERT INTO PARTIDOEQUIPO1 (IDEQUIPO, IDPARTIDO)
-                    VALUES (V_IdEquipo1, V_IdPartido);
-                    
-                    INSERT INTO PARTIDOEQUIPO2 (IDEQUIPO, IDPARTIDO)
-                    VALUES (V_IdEquipo2, V_IdPartido);
+                    INSERT INTO PARTIDOS (HORA, IDEQUIPO1, IDEQUIPO2, IDJORNADA)
+                    VALUES (V_HoraPartido, V_IdEquipo1, V_IdEquipo2, V_IdJornada);
                     
                     V_Emparejados := 0;
                     
@@ -143,8 +101,6 @@ CREATE OR REPLACE PACKAGE BODY DatosCalendario AS
             
             END LOOP;
             
-            
-            
         END LOOP;
         
     END EmparejarLigaRegular;
@@ -159,7 +115,7 @@ CREATE OR REPLACE PACKAGE BODY DatosCalendario AS
     BEGIN
         OPEN C_CURSOR FOR
                 SELECT * FROM EMPAREJAMIENTOS
-                ORDER BY IDJORNADA, EQUIPO1;
+                ORDER BY JORNADA, EQUIPO1;
     END ListaEmparejamientos;
 
 --------------------------------------------------------------------------------
@@ -169,9 +125,78 @@ CREATE OR REPLACE PACKAGE BODY DatosCalendario AS
     PROCEDURE EmparejarPlayOff
     AS
     
+    C_CursorEquipos SYS_REFCURSOR;
+    V_Equipo        EQUIPOS.ID%TYPE;
+    V_Equipo1       EQUIPOS.ID%TYPE;
+    V_Equipo2       EQUIPOS.ID%TYPE;
+    V_Equipo3       EQUIPOS.ID%TYPE;
+    V_Equipo4       EQUIPOS.ID%TYPE;
+    V_Equipo5       EQUIPOS.ID%TYPE;
+    V_Equipo6       EQUIPOS.ID%TYPE;
+    V_Equipo7       EQUIPOS.ID%TYPE;
+    V_Equipo8       EQUIPOS.ID%TYPE;
+    V_IdSplit       SPLIT.ID%TYPE;
+    V_IdJornada     JORNADAS.ID%TYPE;
+    V_Contador      NUMBER := 1;
+    
     BEGIN
     
-        NULL;
+        OPEN C_CursorEquipos FOR
+        SELECT E.ID
+        FROM PARTIDOS P, EQUIPOS E
+        WHERE E.ID = P.IDEQUIPOGANA
+        GROUP BY E.ID
+        ORDER BY COUNT(P.IDEQUIPOGANA);
+        
+    LOOP
+    
+    FETCH C_CursorEquipos INTO V_Equipo;
+    
+    CASE
+    WHEN V_Contador = 1 THEN V_Equipo1 := V_Equipo;
+    WHEN V_Contador = 2 THEN V_Equipo2 := V_Equipo;
+    WHEN V_Contador = 3 THEN V_Equipo3 := V_Equipo;
+    WHEN V_Contador = 4 THEN V_Equipo4 := V_Equipo;
+    WHEN V_Contador = 5 THEN V_Equipo5 := V_Equipo;
+    WHEN V_Contador = 6 THEN V_Equipo6 := V_Equipo;
+    WHEN V_Contador = 7 THEN V_Equipo7 := V_Equipo;
+    WHEN V_Contador = 8 THEN V_Equipo8 := V_Equipo;
+    
+    END CASE;
+    
+    V_Contador := V_Contador +1;
+    
+    EXIT WHEN V_Contador = 8;
+
+    END LOOP;
+    
+    SELECT MAX(ID) INTO V_Split
+    FROM SPLIT;
+    
+    -- Jornada PlayOff 1 --
+    INSERT INTO IDJORNADA (FECHA, TIPO, IDSPLIT)
+    VALUES (SYSDATE, 'playoff', V_Split);
+    
+    SELECT ID INTO V_IdJornada
+    FROM JORNADAS 
+    WHERE ROWNUM = 1 
+    ORDER BY ID DESC;
+    
+    -- Partido PlayOff 1 --
+    INSERT INTO PARTIDOS (HORA, IDEQUIPO1, IDEQUIPO2, IDJORNADA)
+    VALUES ('16:00', V_Equipo1, V_Equipo8, V_IdJornada);
+    
+    -- Partido PlayOff 2 --
+    INSERT INTO PARTIDOS (HORA, IDEQUIPO1, IDEQUIPO2, IDJORNADA)
+    VALUES ('18:00', V_Equipo2, V_Equipo7, V_IdJornada);
+    
+    -- Partido PlayOff 3 --
+    INSERT INTO PARTIDOS (HORA, IDEQUIPO1, IDEQUIPO2, IDJORNADA)
+    VALUES ('18:00', V_Equipo3, V_Equipo6, V_IdJornada);
+    
+    -- Partido PlayOff 4 --
+    INSERT INTO PARTIDOS (HORA, IDEQUIPO1, IDEQUIPO2, IDJORNADA)
+    VALUES ('18:00', V_Equipo4, V_Equipo5, V_IdJornada);
     
     END EmparejarPlayOff;
 
